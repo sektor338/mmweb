87539d6caf617eed04ae9eb04f8d1440b55744a8
Mészáros Martin Zsolt <37712920+sektor338@users.noreply.github.com>
2024-06-14 21:13:38 +0200

    before pages deployment package

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {TimelineComponent} from "../../home/timeline/timeline.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+    imports: [
+        TimelineComponent,
+        RouterLink
+    ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
